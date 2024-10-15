ac34e5d20c28d8d5cb9c692fa241352aa7fe91b1
Ceasar <emperorceasar83@gmail.com>
2024-10-15 15:17:20 -0700

    Corrections???

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-
-def number_of_lines(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        return len(list(f))
+def write_file(filename="", text=""):
+    """Writes a string to a text file and returns the number of characters written."""
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
